12604f7cf3ad02c8ba449aee9d2d900b18b1eb08
bananabits4 <kedarspatil888@gmail.com>
2024-02-04 14:15:09 +0530

    tasks work well now

--- Amongus 3d/Assets/Scripts/TaskLoad.cs
+++ Amongus 3d/Assets/Scripts/TaskLoad.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class Task1 : MonoBehaviour, IInteractable
 {   
     public GameObject task1;
+    public GameObject crosshairs;
 
     public void Interact(){
+        Cursor.lockState = CursorLockMode.None;
         task1.SetActive(true);
+        crosshairs.SetActive(false);
     }
 }
